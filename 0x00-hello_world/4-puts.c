95cf72653c27dc695f9af240d91f83e86e7332a8
RonewaMab <ronewamab9403@gmail.com>
2022-07-03 00:00:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 
-#int main(void){
-puts("\"Programming is like building amultilingual puzzle");
-return (0);
+/**
+ * main-prints the phrase "Programming is like 
+ * Return: 0 if exited properly,non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+  return (0);
 }
--- 0x00-hello_world/6-size.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-/**
- * main .print the size of various types
- * Return: 0
