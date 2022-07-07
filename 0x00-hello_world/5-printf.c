983ae5dd013e432e0205aeafbbaec695f5fcc48a
RonewaMab <ronewamab9403@gmail.com>
2022-07-07 13:41:15 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-prints the phrase with proper grammar,
+ * but the out put is piece of art
+ * Return:0 if exited properly,non-zero otherwise
+ */
+int main(void)
+{
+printf("with propergramma,but the outcome is a piece of art,");
+return (0);
+}
