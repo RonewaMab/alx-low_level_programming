fdf71786ed73bb7761c7da3e856dd24b746b237b
RonewaMab <ronewamab9403@gmail.com>
2022-07-14 10:27:21 -0700

    low level

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string
- * @s: String to count
- * Return: String length
- */
-int _strlen(char *s)
-		
+* _strlen - Returns the length of a string
+* @s: String to count
+* Return: String length
+*/
+
+int _strlen (char *s)	
 {
-  int c = 0;
+int c = 0;
 
-  for(; *s != '\0'; s++)
-     {
-     c++;
-     }
+for (; *s != '\0'; s++)
+{
 
-  return (c);
+c++;
+
+}
+return (c);
+
 }
