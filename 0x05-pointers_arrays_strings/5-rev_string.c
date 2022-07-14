4c23c3b0c93d4598fd037b2ba910737a9f0df866
RonewaMab <ronewamab9403@gmail.com>
2022-07-14 11:22:49 -0700

    low level

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,36 +16,20 @@ void rev_string(char *s)
 
 	char *a, aux;
 
-
-
 	a = s;
 
-
-
 	while (s[c] != '\0')
-
 	{
-
 		c++;
-
 	}
-
-
 
 	for (k = 1; k < c; k++)
-
 	{
-
 		a++;
-
 	}
 
-
-
 	for (i = 0; i < (c / 2); i++)
-
 	{
-
 		aux = s[i];
 
 		s[i] = *a;
@@ -53,9 +37,5 @@ void rev_string(char *s)
 		*a = aux;
 
 		a--;
-
 	}
-
 }
-
-
